ea24958183205e242a50b708b2ea60b7d3a1e1a1
53Abdeali <aliabdealifakhri53@gmail.com>
2024-08-13 17:46:36 +0530

    Docker and NGINX started.

---
+++ Travel_Itinerary/Travel_Itinerary/src/main/java/com/travel/Travel_Itinerary/TravelItineraryApplication.java
@@ -0,0 +1,13 @@
+package com.travel.Travel_Itinerary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelItineraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelItineraryApplication.class, args);
+	}
+
+}
---
+++ Travel_Itinerary/Travel_Itinerary/src/test/java/com/travel/Travel_Itinerary/TravelItineraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.Travel_Itinerary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelItineraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
