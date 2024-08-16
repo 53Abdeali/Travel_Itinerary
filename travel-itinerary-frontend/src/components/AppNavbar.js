64680ace91cdbbc3e86f485eacfbfc42e8fbd17e
53Abdeali <aliabdealifakhri53@gmail.com>
2024-08-16 23:59:11 +0530

    Loader and Navbar added.

---
+++ travel-itinerary-frontend/src/components/AppNavbar.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import './Styles/AppNavbar.css'
+import {GiHamburgerMenu} from 'react-icons/gi'
+
+function AppNavbar() {
+  const [showMediaIcon, setShowMediaIcon] = useState(false);
+  return (
+    <nav className="main-nav">
+      <div className="logo">
+        {/* <img src="path-to-your-logo-image" alt="Logo" /> */}
+        <h2><span>B</span>ohra
+        <span> I</span>tinerary</h2>
+      </div>
+      <div className={showMediaIcon ? "menu-link mobile-menu-link" : "menu-link"}>
+        <ul>
+        <li><a href="/" className="navbar-link">Home</a></li>
+        <li><a href="/explore" className="navbar-link">Explore Destinations</a></li>
+        <li><a href="/journey" className="navbar-link">Plan a Journey</a></li>
+        <li><a href="/about" className="navbar-link">About Us</a></li>
+        <li><a href="/contact" className="navbar-link">Contact Us</a></li>
+        </ul>
+      </div>
+
+      <div className="login">
+        <ul>
+          <li><a href="/login" className="navbar-login-link">Login/Register</a></li>
+        </ul>
+        <div className='hamburger-menu'>
+              <a href='/#' onClick={() => setShowMediaIcon(!showMediaIcon)}>
+                <GiHamburgerMenu style = {{color:'darkgreen'}}/>
+              </a>
+        </div>
+      </div>
+      
+    </nav>
+  );
+}
+
+export default AppNavbar;
---
+++ travel-itinerary-frontend/src/components/WelcomeLoader.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import './Styles/WelcomeLoader.css';
+
+const WelcomeLoader = ({ onComplete }) => {
+  const [text, setText] = useState('');
+  const fullText = "أهلا و سهلا"; // Your phrase
+  const words = fullText.split(' '); // Split the phrase into words
+  const [index, setIndex] = useState(0); // Track the current word index
+
+  useEffect(() => {
+    if (index < words.length) {
+      const timeout = setTimeout(() => {
+        setText((prev) => prev + (prev ? ' ' : '') + words[index]);
+        setIndex(index + 1);
+      }, 450); // Adjust the timing as needed
+
+      return () => clearTimeout(timeout);
+    } else {
+      const timeout = setTimeout(() => {
+        if (onComplete) onComplete();
+      }, 3500); // Delay before ending the loader
+
+      return () => clearTimeout(timeout);
+    }
+  }, [index, onComplete, words]);
+
+  return (
+    <div className="welcome-loader">
+      <h1 className="welcome-text">
+        {text}
+        <span className="underline"></span>
+      </h1>
+    </div>
+  );
+};
+
+export default WelcomeLoader;
