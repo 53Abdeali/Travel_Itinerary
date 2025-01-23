205b15beaac2a121b949d0a3213cfa40b9fdb587
53Abdeali <aliabdealifakhri53@gmail.com>
2025-01-23 18:13:02 +0530

    JWT Authentication

--- travel-itinerary-frontend/src/components/Login.js
+++ travel-itinerary-frontend/src/components/Login.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import web from "./Images/website logo.jpg"
 import "./Styles/Login.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
